c7c42d67a304e58e61ac84fd2731e51fb01ef2eb
Lucas Carlos Soares Fernandes <lucazcarloz@gmail.com>
2022-09-28 11:44:38 -0300

    Código primeiro desafio

--- FirstChallenger.js
+++ FirstChallenger.js
@@ -1,1 +1,22 @@
+let numeroUm=1, stringUm='1', numeroTrinta=30, stringTrinta='30', numeroDez=10, stringDez='10';
 
+if (numeroUm == stringUm) {
+    console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes');
+  } 
+    else {
+        console.log('As variáveis numeroUm e stringUm não tem o mesmo valor');
+  }
+
+if (numeroTrinta === stringTrinta) {
+    console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo');
+} 
+    else {
+        console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo');
+  }
+
+if (numeroDez == stringDez) {
+    console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes');
+  } 
+    else {
+        console.log('As variáveis numeroDez e stringDez não tem o mesmo valor');
+  }
